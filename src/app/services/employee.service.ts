a263aeb418784ac38084e051dd4643611499a342
ksimali <cassim.ali@outlook.fr>
2025-08-12 18:12:29 -0400

    Implement employee.service.ts to handle employee crud operations

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,8 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Employee } from '../models/employee.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
+  private apiServerUrl = '';
+
+  constructor(private http: HttpClient){}
+
+  // Method to get all employees
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+  }
+
+  // Method to add an employee in the database
+  public addEmployee(employee : Employee) : Observable<Employee>{
+    return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+  }
+
+  // Method to update an employee
+  public updateEmployee(employee : Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+  }
+
+  // Method to delete an employee
+  public deleteEmployee(employeeId : number) : Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`,);
+  }
 }
+
