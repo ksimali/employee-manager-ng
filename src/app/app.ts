a55fc38256e753da3e567e7ed611e8f85e75210c
ksimali <cassim.ali@outlook.fr>
2025-08-12 21:31:46 -0400

    Display data in app component : - get all employees data using employeeService in app.ts file - Add provideHttpClient module in main.ts file - display data in app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeService } from './services/employee.service';
+import { Employee } from './models/employee.model';
+import { HttpErrorResponse, provideHttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected title = 'employee-manager-ng';
+export class App implements OnInit {
+  
+  // property
+  public employees: Employee[] = [];
+
+  // constructor with the employee.service injection
+  constructor(private employeeService: EmployeeService) {}
+  
+  // to call getEmployees whenever this component is loaded
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+ 
+  // Method to get all employees
+  public getEmployees(){
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error:HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }
