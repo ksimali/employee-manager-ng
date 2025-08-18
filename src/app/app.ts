685a196d74629697e419c436a28d1ca98ac89b7b
ksimali <cassim.ali@outlook.fr>
2025-08-18 14:32:30 -0400

    Implement searchbar feature : - Create searchEmployees() method in app.ts -Add a click event listener in app.html searchBar input

--- src/app/app.ts
+++ src/app/app.ts
@@ -109,4 +109,21 @@ export class App implements OnInit {
       }
     );
   }
+
+  // Method to search employees
+  public searchEmployees(key:string): void {
+    const results: Employee[] = [];
+    for(const employee of this.employees) {
+      if(employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        results.push(employee);
+      }
+    }
+    this.employees = results;
+    if(results.length === 0 || !key) {
+      this.getEmployees();
+    }
+  }
 }
